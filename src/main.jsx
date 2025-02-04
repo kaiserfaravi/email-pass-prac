b1f221e0650d1b444920ef65adced5008c632cf4
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-02-04 17:27:14 +0600

    form updated

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from './Components/Root.jsx';
+import Home from './Components/Home.jsx';
+import Registration from './Registration.jsx';
+import Login from './Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/registration',
+        element:<Registration></Registration>
+      },{
+        path:'/login',
+        element:<Login></Login>
+      }
+    ]
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
